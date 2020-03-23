63afe00661daf83861b6543ab96885dd01a2f783
David Oliver <davooliver1964@gmail.com>
2020-03-23 10:53:50 -0400

    project creation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {/* TODO */}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+
+    renderSquare(i) {
+        return <Square />;
+    }
+
+    render() {
+        // Use two loops to make squares 
+        // Allow board size to be flexible
+        let numColumns = 3;
+        let numRows = 3;
+        let tiles = [];
+        for (let i = 0; numRows < i; i++) {
+            let row = [];
+            for (let j = 0; numColumns < j; j++) {
+                row.push(this.renderSquare(i * numColumns + j));
+            }
+            tiles.push(<div className="board-row">{row}</div>);
+        }
+        console.log({ tiles });
+
+        return (
+            <div>
+                {tiles}
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
